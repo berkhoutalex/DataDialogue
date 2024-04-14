d979cde867139f0c998093087e46170851811446
Alex Berkhout <alexander.berkhout@marquetteenergyanalytics.com>
2024-04-13 22:41:29 -0600

    setup initial ui

--- src/datadialogue/sample_data/timeseries.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import pandas_datareader as pdr
-import yfinance as yf
-
-def aapl_data(start_date, end_date):
-  shares_df = yf.download('AAPL', start=start_date, end=end_date)
-  return shares_df
